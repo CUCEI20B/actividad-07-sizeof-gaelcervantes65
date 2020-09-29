803c86cba24261d5c9b5a12a1647bafbbab7be2c
Gael Cervantes <oswaldo.cervantes7474@alumnos.udg.mx>
2020-09-29 16:28:43 -0500

    Funciona el codigo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+
+    cin >> cadena;
+
+    if(cadena == "char"){
+        cout << sizeof(char);
+    }
+
+    else if(cadena == "int"){
+        cout << sizeof(int);
+    }
+
+    else if(cadena == "short"){
+        cout << sizeof(short);
+    }
+
+    else if(cadena == "float"){
+        cout << sizeof(float);
+    }
+
+    else if(cadena == "double"){
+        cout << sizeof(double);
+    }
+
+    else if(cadena == "long"){
+        cout << sizeof(long);
+    }
 
     return 0;
 }
